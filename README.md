3e10a5bbaa0d57904f60b78587be9df75b4375f3
Larry Wang <larry@nebulas.io>
2018-10-10 15:30:59 +0800

    add nbre

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # homebrew-nebulas
+
+=================
+
+Homebrew Tap for Nebulas
+
+
+## Installation
+
+```
+brew tap nebulasio/nebulas
+```
+
+### Go client(TODO)
+```
+brew install nebulas
+```
+
+### nbre client
+```
+brew install nbre
+```
---
+++ nbre.rb
@@ -0,0 +1,38 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Nbre < Formula
+  desc "nebulas nbre runtime environment"
+  homepage "https://github.com/nebulasio/go-nebulas"
+  url "https://s3-us-west-1.amazonaws.com/develop-center/nbre/nbre.tar.gz"
+  version "0.1.1"
+  sha256 "8e70164ef410056042c157ea2511cc108a112341c688b1c38a41608ac454870f"
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # system "./configure", "--disable-debug",
+    #                       "--disable-dependency-tracking",
+    #                       "--disable-silent-rules",
+    #                       "--prefix=#{prefix}"
+    # # system "cmake", ".", *std_cmake_args
+    # system "make", "install" # if this fails, try separate make/make install steps
+    # system "tar -zxvf nbre.tar.gz"
+    bin.install Dir["bin/*"]
+    lib.install Dir["lib/*"]
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test nbre`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
